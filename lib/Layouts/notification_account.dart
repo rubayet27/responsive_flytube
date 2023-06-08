9f193acc893baf579c2058ca5b4483a465c84429
rubayet27 <rubayetrr27@gmail.com>
2023-06-09 02:50:20 +0600

    Notification Center and Account center

--- lib/Layouts/notification_account.dart
+++ lib/Layouts/notification_account.dart
@@ -13,6 +13,42 @@ class NotificationCenter extends StatelessWidget {
           borderRadius: BorderRadius.circular(20),
           color: Colors.black87,
         ),
+        child: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.only(left: 20, top: 20),
+              alignment: Alignment.topLeft,
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  topRight: Radius.circular(20),
+                ),
+              ),
+              child: const Text(
+                "Notifications",
+                style: TextStyle(fontSize: 18),
+              ),
+            ),
+            const Divider(),
+            Expanded(
+              child: Container(
+                decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(20),
+                        bottomRight: Radius.circular(20))),
+                padding: const EdgeInsets.only(bottom: 14),
+                child: ListView.builder(
+                  itemCount: 30,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text("$index"),
+                    );
+                  },
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -25,8 +61,8 @@ class AccountCenter extends StatelessWidget {
       shadowColor: Colors.red,
       alignment: Alignment.topRight,
       content: Container(
-        width: 350,
-        height: 500,
+        width: 300,
+        height: 400,
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(20),
           color: Colors.black87,
@@ -35,15 +71,15 @@ class AccountCenter extends StatelessWidget {
           children: [
             Container(
               margin: EdgeInsets.only(top: 10),
-              height: 170,
-              width: 170,
+              height: 120,
+              width: 120,
               decoration: const BoxDecoration(
                 shape: BoxShape.circle,
                 color: Colors.grey,
               ),
               child: const Icon(
                 Icons.account_circle,
-                size: 170,
+                size: 120,
                 color: Colors.black,
               ),
             ),
@@ -54,24 +90,27 @@ class AccountCenter extends StatelessWidget {
                   padding: EdgeInsets.only(top: 10),
                   child: Text(
                     "rubayet27",
-                    style: TextStyle(fontSize: 25),
+                    style: TextStyle(fontSize: 22),
                   ),
                 ),
               ],
             ),
-            const ListTile(
-              title: Text(
+            const Divider(),
+            ListTile(
+              onTap: () {},
+              title: const Text(
                 "Settings",
-                style: TextStyle(fontSize: 20),
+                style: TextStyle(fontSize: 17),
               ),
-              leading: Icon(Icons.settings),
+              leading: const Icon(Icons.settings),
             ),
-            const ListTile(
-              title: Text(
+            ListTile(
+              onTap: () {},
+              title: const Text(
                 "Log out",
-                style: TextStyle(fontSize: 20),
+                style: TextStyle(fontSize: 17),
               ),
-              leading: Icon(Icons.logout),
+              leading: const Icon(Icons.logout),
             ),
           ],
         ),
